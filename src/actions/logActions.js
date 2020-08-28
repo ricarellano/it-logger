7726392684ba797e2c6568874d0c2e898b9c9434
ricarellano <rarellanob@gmail.com>
2020-08-28 15:03:46 -0700

    add redux to logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,11 +1,11 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from './types';
 
 // Get logs from server
 export const getLogs = () => async dispatch => {
       try {
           setLoading();
 
-          const res = await fetch('./logs');
+          const res = await fetch('/logs');
           const data = await res.json();
 
           dispatch({
@@ -15,10 +15,103 @@ export const getLogs = () => async dispatch => {
       } catch (error) {
           dispatch({
               type: LOGS_ERROR,
-              payload: error.response.data
+              payload: error.response.statusText
           })
           
       }
+}
+
+// Add new log
+export const addLog = (log) => async dispatch => {
+      try {
+          setLoading();
+
+          const res = await fetch('/logs', {
+              method: 'POST',
+              body: JSON.stringify(log),
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          });
+
+          const data = await res.json();
+
+          dispatch({
+              type: ADD_LOG,
+              payload: data
+          })
+      } catch (error) {
+          dispatch({
+              type: LOGS_ERROR,
+              payload: error.response.statusText
+          })
+          
+      }
+}
+
+// Delete log from server
+export const deleteLog = (id) => async dispatch => {
+    try {
+        setLoading();
+
+        await fetch(`/logs/${id}`, {
+            method: 'DELETE'
+        });
+
+        dispatch({
+            type: DELETE_LOG,
+            payload: id
+        })
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.statusText
+        })
+        
+    }
+} 
+
+// Update log on server
+export const updateLog = (log) => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await res.json();
+
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.statusText
+        })
+        
+    }
+} 
+
+// Set current log
+export const setCurrent = log => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+}
+
+// Clear current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT
+    }
 }
 
 
--- src/reduders/logReducer.js
+++ src/reduders/logReducer.js
@@ -1,32 +1,69 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
+} from "../actions/types";
 
 const initialState = {
-    logs: null,
-    current: null,
-    loading: false,
-    error: null
-}
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
 
 export default (state = initialState, action) => {
-    switch(action.type) {
-        case GET_LOGS:
-            return {
-                ...state,
-                logs: action.payload,
-                loading: false
-            };
-        case SET_LOADING:
-            return {
-                ...state,
-                loading: true
-            };
-            case LOGS_ERROR:
-                console.error(action.payload);
-                return {
-                    ...state,
-                    error: action.payload
-                };
-        default:
-            return state;
-    };
-}
+  switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
+        loading: false,
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((log) => log.id !== action.payload),
+        loading: false,
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((log) => log.id !== action.payload),
+        loading: false,
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
